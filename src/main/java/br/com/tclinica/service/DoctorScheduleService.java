fadfbc8b499ddc2ae14d0626a27ad9aa333a14ab
sapiensmagno <sapiensmagno@bol.com.br>
2017-09-01 10:36:22 -0300

    created DoctorScheduleService.

---
+++ src/main/java/br/com/tclinica/service/DoctorScheduleService.java
@@ -0,0 +1,14 @@
+package br.com.tclinica.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class DoctorScheduleService {
+
+    private final Logger log = LoggerFactory.getLogger(DoctorScheduleService.class);
+
+}
