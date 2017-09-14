70a96462af249e43b1f787be9e55c212ef1b1d09
sapiensmagno <sapiensmagno@bol.com.br>
2017-09-14 17:33:01 -0300

    Translate java enum days of week

--- src/main/webapp/app/entities/available-weekdays/available-weekdays-tcc.state.js
+++ src/main/webapp/app/entities/available-weekdays/available-weekdays-tcc.state.js
@@ -26,6 +26,7 @@
             resolve: {
                 translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
                     $translatePartialLoader.addPart('availableWeekdays');
+                    $translatePartialLoader.addPart('DayOfWeek');
                     $translatePartialLoader.addPart('global');
                     return $translate.refresh();
                 }]
