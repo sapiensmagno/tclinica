bc95d0d8df4cbdd5a014a8c4f0c6143e726050da
sapiensmagno <sapiensmagno@bol.com.br>
2017-09-19 03:07:28 -0300

    include role_receptionist on dialog

--- src/main/webapp/app/admin/user-management/user-management-dialog.controller.js
+++ src/main/webapp/app/admin/user-management/user-management-dialog.controller.js
@@ -10,7 +10,7 @@
     function UserManagementDialogController ($stateParams, $uibModalInstance, entity, User, JhiLanguageService) {
         var vm = this;
 
-        vm.authorities = ['ROLE_USER', 'ROLE_ADMIN', 'ROLE_DOCTOR', 'ROLE_ACCOUNTANT'];
+        vm.authorities = ['ROLE_USER', 'ROLE_ADMIN', 'ROLE_DOCTOR', 'ROLE_ACCOUNTANT', 'ROLE_RECEPTIONIST'];
         vm.clear = clear;
         vm.languages = null;
         vm.save = save;
